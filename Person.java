112c5532f2db0ced76bd7735119443c8c543ea45
Jingga Hagane <39108214+fahruroze@users.noreply.github.com>
2019-01-08 13:11:42 +0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rumahsakit.entity;
+
+/**
+ *
+ * @author Ninda Auliya
+ */
+public class Person {
+    private String nama;
+    private String alamat;
+    private String jenis_kelamin;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }        
+
+    public String getJenis_kelamin() {
+        return jenis_kelamin;
+    }
+
+    public void setJenis_kelamin(String jenis_kelamin) {
+        this.jenis_kelamin = jenis_kelamin;
+    }
+   
+}
